f892048866e0d05aa3eb741d2c1c1177991768db
derekpyoung <59941613+derekpyoung@users.noreply.github.com>
2020-06-11 12:46:44 -0700

    cannot click same cell twice and place x and o

--- ticTac.js
+++ ticTac.js
@@ -1,58 +1,72 @@
 const numberButtons = document.querySelectorAll('[data-cell]')
-// const winningCombos = [
-//   [0,1,2]
-//   [3,4,5]
-//   [6,7,8]
-//   [0,4,8]
-//   [2,4,6]
-//   [0,3,6]
-//   [1,4,7]
-//   [2,5,8]];
 
-var activePlayer = 0;
-var red = [];
-var blue = [];
-
-function colorPicker(){ //changes color for player/switches player
-
-  if(activePlayer == 0){
-    numberButtons.forEach(button => {
-      button.addEventListener('click', () => {
-      button.style.backgroundColor = 'red'
-      red.push(button.innerHTML);
-      return red;
-      })
-
-
-      activePlayer = 1;
-      colorPicker(poops);
-
-    });
-  }else{
-    numberButtons.forEach(button => {
-      button.addEventListener('click', () => {
-      button.style.backgroundColor = 'blue'
-      blue.push(button.innerHTML);
-      console.log('blue' + blue);
-      activePlayer = 0;
-      colorPicker(poops);
-      })
-    });
-  }
-  return poops;
+function putAnX(button) {
+  button.innerHTML = 'X';
 }
-colorPicker(activePlayer);
-
-function redArray(activePlayer){
-  if(activePlayer == 0){
-    red.push(button.innerHTML);
-    console.log(red);
-  } else {
-    colorPicker();
-  }
+function clickButton(button) {
+  putAnX(button);
+  changePlayer();
 }
+function setUpEventListeners(){
+  numberButtons.forEach(button => {
+    button.addEventListener('click', () => { clickButton(button) })
+  })
+}
+setUpEventListeners();
+// // const winningCombos = [
+// //   [0,1,2]
+// //   [3,4,5]
+// //   [6,7,8]
+// //   [0,4,8]
+// //   [2,4,6]
+// //   [0,3,6]
+// //   [1,4,7]
+// //   [2,5,8]];
 //
-
+// var activePlayer = 0;
+// var red = [];
+// var blue = [];
+//
+// function colorPicker(){ //changes color for player/switches player
+//
+//   if(activePlayer == 0){
+//     numberButtons.forEach(button => {
+//       button.addEventListener('click', () => {
+//       button.style.backgroundColor = 'red'
+//       red.push(button.innerHTML);
+//       return red;
+//       })
+//
+//
+//       activePlayer = 1;
+//       colorPicker(poops);
+//
+//     });
+//   }else{
+//     numberButtons.forEach(button => {
+//       button.addEventListener('click', () => {
+//       button.style.backgroundColor = 'blue'
+//       blue.push(button.innerHTML);
+//       console.log('blue' + blue);
+//       activePlayer = 0;
+//       colorPicker(poops);
+//       })
+//     });
+//   }
+//   return poops;
+// }
+// colorPicker(activePlayer);
+//
+// function redArray(activePlayer){
+//   if(activePlayer == 0){
+//     red.push(button.innerHTML);
+//     console.log(red);
+//   } else {
+//     colorPicker();
+//   }
+// }
+// //
+//
 
 
 
--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,54 @@
 const numberButtons = document.querySelectorAll('[data-cell]')
+var player = 'X';
+let circleTurn = true;
 
-function putAnX(){
-  numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-    button.innerHTML = 'X';
+function setUpEventListeners(){
+  numberButtons.forEach(function(currentNumberButton){
+    currentNumberButton.addEventListener('click', () => {
+      tictactoe(currentNumberButton);
     })
   })
 }
-putAnX();
+function isMarked(currentNumberButton){
+  if(currentNumberButton.innerHTML === "X" || currentNumberButton.innerHTML === "O"){
+    return true;
+  } else {
+    return false;
+  }
+}
+function putAnX(currentNumberButton){
+  currentNumberButton.innerHTML = 'X';
+}
+
+function putAnO(currentNumberButton){
+   currentNumberButton.innerHTML = 'O';
+}
+
+function changePlayer(){
+  circleTurn = !circleTurn
+}
+
+function tictactoe(clickedCell){
+  if(isMarked(clickedCell)){
+    console.log('poop');
+  } else {
+    if (circleTurn){
+      putAnO(clickedCell);
+      changePlayer();
+    } else {
+      putAnX(clickedCell);
+      changePlayer();
+    }
+  }
+
+}
+setUpEventListeners();
+
+
+//
+// function eventListening(){
+//   numberButtons.forEach(button => {
+//     button.addEventListener('click', () => clickButton(button));
+//   })
+// }
+//eventListening();
